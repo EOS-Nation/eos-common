a1b8e0a0901c92238503c62ae8ab181380701776
Denis <carriere.denis@gmail.com>
2020-02-24 19:51:19 -0500

    add rollup

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from "./eosiolib/asset";
+export * from "./eosiolib/check";
 export * from "./eosiolib/symbol";
 export * from "./eosiolib/time";
-export * from "./eosiolib/voting";
 export * from "./eosiolib/utils";
+export * from "./eosiolib/voting";
