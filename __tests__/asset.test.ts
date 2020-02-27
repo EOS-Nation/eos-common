160a7c20061fe3eacfca05927a9341489a7395bf
Denis <carriere.denis@gmail.com>
2020-02-27 14:46:49 -0500

    add tests

--- __tests__/asset.test.ts
+++ __tests__/asset.test.ts
@@ -17,12 +17,19 @@ test(`comparison works as expected`, () => {
 });
 
 test("decimal conversions work as expected", () => {
-    const amount1 = new Asset(10000, EOS);
-    expect(asset_to_number(amount1)).toBe(1.0000);
+    const quantity1 = new Asset(10000, EOS);
+    expect(asset_to_number(quantity1)).toBe(1.0000);
+    expect(quantity1.amount).toBe(10000n);
 
-    const amount2 = new Asset('911.7285 EOS');
-    expect(asset_to_number(amount2)).toBe(911.7285);
+    const quantity2 = new Asset('911.7285 EOS');
+    expect(asset_to_number(quantity2)).toBe(911.7285);
+    expect(quantity2.amount).toBe(9117285n);
 
-    const amount3 = new Asset(100000000, BTC);
-    expect(asset_to_number(amount3)).toBe(1.00000000);
+    const quantity3 = new Asset(100000000, BTC);
+    expect(asset_to_number(quantity3)).toBe(1.00000000);
+    expect(quantity3.amount).toBe(100000000n);
+
+    const quantity4 = new Asset("9643.4600 USD");
+    expect(asset_to_number(quantity4)).toBe(9643.4600);
+    expect(quantity4.amount).toBe(96434600n);
 });
