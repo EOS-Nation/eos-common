0cca30d371d91b4f76826b28f6ede607e2bb62d5
Denis <carriere.denis@gmail.com>
2020-03-15 16:44:01 -0400

    remove utils tests

--- __tests__/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { isNull } from "..";
-
-test("utils.isNull", () => {
-  expect( isNull( undefined )).toBeTruthy();
-  expect( isNull( null )).toBeTruthy();
-  expect( isNull( "foo" )).toBeFalsy();
-  expect( isNull( 0 )).toBeFalsy();
-});
-
