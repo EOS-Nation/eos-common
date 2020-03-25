33d6160a69a0851d70d6781e5742183bad4598c7
Denis <carriere.denis@gmail.com>
2020-03-25 16:45:17 -0400

    update symbol_code to bigInteger

--- eosiolib/symbol_code_bigint.ts
+++ eosiolib/symbol_code_bigint.ts
@@ -1,21 +1,23 @@
 import { check } from "./check";
-import bigInt, { BigInteger } from "big-integer";
 
 /**
  * @class Stores the symbol code
  * @brief Stores the symbol code as a uint64_t value
  */
 export class SymbolCode {
+    get [Symbol.toStringTag](): string {
+        return 'symbol_code';
+    }
     public typeof(): string { return 'symbol_code' }
 
-    private value = bigInt(0);
+    private value = BigInt(0);
 
     // constructor()
-    constructor( str?: string | number | BigInteger ) {
+    constructor( str?: string | number | bigint ) {
         if (typeof str == "number" || typeof str == 'bigint') {
-            this.value = bigInt(str);
-        } else if ( str && typeof str == "string" ) {
-            let value = bigInt(0);
+            this.value = BigInt(str);
+        } else if ( str ) {
+            let value = BigInt(0);
             if ( str.length > 7 ) {
                 check( false, "string is too long to be a valid symbol_code" );
             }
@@ -23,63 +25,62 @@ export class SymbolCode {
                 if ( itr < 'A' || itr > 'Z') {
                     check( false, "only uppercase letters allowed in symbol_code string" );
                 }
-                value = value.shiftLeft(bigInt(8));
-                value = value.or(bigInt(itr.charCodeAt(0)));
+                value <<= BigInt(8);
+                value |= BigInt(itr.charCodeAt(0));
             }
             this.value = value;
-        } else if ( typeof str == "object") {
-            this.value = str;
         }
     }
 
-    public raw(): BigInteger {
+    public raw(): bigint {
         return this.value;
     }
 
     public isTruthy(): boolean {
-        return this.value.notEquals(0);
+        return this.value != BigInt(0);
     }
 
     public isFalsy(): boolean {
-        return this.value.equals(0);
+        return this.value == BigInt(0);
     }
 
     public length(): number {
-        let sym = this.value;
+        let sym = BigInt(this.value);
         let len = 0;
         while (Number(sym) & 0xFF && len <= 7) {
            len++;
-           sym = sym.shiftRight(8);
+           sym >>= BigInt(8);
         }
         return len;
      }
 
-     public to_string(): string  {
-        const mask = bigInt("0x00000000000000FF");
-        if (this.value.equals(0)) return '';
+    public to_string(): string  {
+        const mask = BigInt(0x00000000000000FF);
+        if (this.value == BigInt(0)) return '';
 
         let begin = "";
-        let v = bigInt(this.value);
-        for( let i = 0; i < 7; ++i, v = v.shiftRight(8) ) {
-            if ( v.equals(0) ) return begin;
-            begin += String.fromCharCode(Number(v.and(mask)));
+        let v = BigInt(this.value);
+        for( let i = 0; i < 7; ++i, v >>= BigInt(8) ) {
+            if ( v == BigInt(0) ) return begin;
+            begin += String.fromCharCode(Number(v & mask));
         }
 
         return begin;
     }
 
     public is_valid(): boolean {
-        let sym = bigInt(this.value);
+        let sym = BigInt(this.value);
         for ( let i = 0; i < 7; i++ ) {
-           const c = String.fromCharCode(Number(sym.and(0xFF)));
+           const c = String.fromCharCode(Number(sym & BigInt(0xFF)));
            if ( !("A" <= c && c <= "Z") ) return false;
-           sym = sym.shiftRight(8);
-           if ( sym.and(0xFF).equals(0) ) {
-                do {
-                    sym = sym.shiftRight(8);
-                    if ( sym.and(0xFF).notEquals(0) ) return false;
-                    i++;
-                } while( i < 7 );
+           sym >>= BigInt(8);
+
+           if ( !(sym & BigInt(0xFF)) ) {
+              do {
+                 sym >>= BigInt(8);
+                 if ( sym & BigInt(0xFF) ) return false;
+                 i++;
+              } while( i < 7 );
            }
         }
         return true;
@@ -91,7 +92,7 @@ export class SymbolCode {
      * @return boolean - true if both provided symbol_codes are the same
      */
     public isEqual(comparison: SymbolCode): boolean {
-        return comparison.value.equals(this.value);
+        return comparison.value === this.value;
     }
 
     /**
@@ -100,7 +101,7 @@ export class SymbolCode {
      * @return boolean - true if both provided symbol_codes are not the same
      */
     public isNotEqual(comparison: SymbolCode): boolean {
-        return comparison.value.notEquals(this.value);
+        return comparison.value !== this.value;
     }
 
     /**
@@ -109,10 +110,10 @@ export class SymbolCode {
      * @return boolean - true if symbol_code `a` is less than `b`
      */
     public isLessThan(comparison: SymbolCode): boolean {
-        return this.value.lesser(comparison.value);
+        return this.value < comparison.value;
     }
 }
 
-export function symbol_code( str?: string | number | BigInteger ): SymbolCode {
+export function symbol_code( str?: string | number | bigint ): SymbolCode {
     return new SymbolCode(str);
 }
