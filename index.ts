10793e4323a7ce8f6bfbc2a9dbd282c10440e499
Denis <carriere.denis@gmail.com>
2020-03-26 23:37:21 -0400

    remove utils from eos-common

--- index.ts
+++ index.ts
@@ -6,6 +6,5 @@ export * from "./eosiolib/symbol_code";
 export * from "./eosiolib/extended_symbol";
 export * from "./eosiolib/time";
 export * from "./eosiolib/name";
-export * from "./eosiolib/utils";
 export * from "./eosiolib/voting";
 export * from "./eosiolib/eosiolib";
